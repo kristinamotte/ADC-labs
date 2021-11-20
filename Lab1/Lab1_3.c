39257bddcc5c27968e1aa98f67e42c8402ad426a
Kristina Motte <krmot@telenor.dk>
2021-11-20 19:44:48 +0200

    Add third task

---
+++ Lab1/Lab1_3.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define N 3
+
+int main() {
+    int firstMatrix[N][N];
+    int secondMatrix[N - 1][N - 1];
+
+    for(int i = 0; i < N; i++) {
+        for(int j = 0; j < N; j++) {
+            firstMatrix[i][j] = rand() % 100;
+        }
+    }
+
+    for(int i = 0; i < N; i++) {
+        for(int j = 0; j < N; j++) {
+            secondMatrix[i][j] = firstMatrix[i][i] + firstMatrix[j][j];
+        }
+        printf("\n");
+    }
+
+    for(int i = 0; i < N; i++) {
+        for(int j = 0; j < N; j++) {
+            printf("%d\t", secondMatrix[i][j]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
