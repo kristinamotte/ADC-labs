3fa2305a227ff14df1a3f4a73641a5c318ab07ec
Kristina Motte <krmot@telenor.dk>
2021-12-11 11:15:52 +0200

    fix lab 1 3

--- Lab1/Lab1_3.c
+++ Lab1/Lab1_3.c
@@ -5,7 +5,7 @@
 
 int main() {
     int firstMatrix[N][N];
-    int secondMatrix[N - 1][N - 1];
+    int secondMatrix[N][N];
 
     for(int i = 0; i < N; i++) {
         for(int j = 0; j < N; j++) {
